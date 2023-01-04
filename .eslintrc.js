db456de08895a0a49527e62ea8961e2f4b547383
kimm950 <kimuo950@gmail.com>
2023-01-04 13:32:51 +0900

    set up project

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    "es2021": true,
+  }
+};
